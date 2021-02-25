47595847aa190f71010a29909a30fbc105eb17d9
Lache Melvin <lache.melvin@gmail.com>
2021-02-25 22:02:11 +1300

    maatau -> matatau

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): React.ReactElement {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mātau incoming!
+          matatau incoming!
         </p>
         <p>
           It&apos;s a React app ;)
