5d96a1b324075cfffdd6a267068cfb62080709f1
Don Smith <hi@dons.dev>
2021-02-28 22:42:46 +1300

    start signin component

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import SignIn from "./SignIn";
+
 function App(): React.ReactElement {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          matatau incoming!
-        </p>
-        <p>
-          It&apos;s a React app ;)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>matatau incoming!</p>
+        <p>It&apos;s a React app ;)</p>
+        <SignIn />
       </header>
     </div>
   );
---
+++ src/SignIn.tsx
@@ -0,0 +1,55 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import "./SignIn.css";
+
+function SignIn(): React.ReactElement {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setForm({
+      username: "",
+      password: "",
+    });
+  }
+
+  return (
+    <div className="SignIn">
+      <p>Sign in to Matatau</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          type="password"
+          required
+        />
+
+        <button>Sign in</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
